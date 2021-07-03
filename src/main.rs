885e9d75eab28c0c3fcef02df9952c9832296f90
AliusDieMorietur <ikamqwerty@gmail.com>
2021-07-03 13:28:11 +0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{fs, path::PathBuf};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "lrs")]
+struct Opt {
+  #[structopt(parse(from_os_str))]
+  input: Option<PathBuf>,
+}
+
+fn run_file(path: PathBuf) {
+  let error = format!("Failed to open {:?}", path);
+  let content =
+    fs::read_to_string(path).expect(&error);
+  println!("{}", content);
+}
+
 fn main() {
-    println!("Hello, world!");
+  let opt = Opt::from_args();
+  match opt.input {
+    Some(f) => {
+      run_file(f);
+    },
+    None => {
+      println!("run_prompt");
+    },
+  }
 }
